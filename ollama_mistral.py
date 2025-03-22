1a5c89d2f77870511b263bd3be452728708ecd7d
Angello5 <angello.llerena@usil.pe>
2025-03-22 13:04:02 -0500

    Pruebas de txt

--- ollama_mistral.py
+++ ollama_mistral.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_documents_loaders import TextLoader
+from langchain.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
